2ff15150e78013a836423e7bb9cbf884df0b0433
himanshu06d <hd156681@gmail.com>
2020-02-22 00:57:21 +0530

    subtraction of two numbres

--- diff.c
+++ diff.c
@@ -2,11 +2,11 @@
 
 void main()
 {
-    int a,b,c;
+    int a,b,c; \\initialize the variables
     printf("enter two numbres : ");
     scanf("%d%d",&a,&b);
     c=a-b;
-    printf("difference of two numbres is : %d",c);
+    printf("difference of two numbres is : %d",c); \\print final c 
     
 
 }
