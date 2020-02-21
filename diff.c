6638ceed281e47c26998d2d0ee2d30c00b7f3a19
himanshu06d <hd156681@gmail.com>
2020-02-21 22:23:56 +0530

    third push

---
+++ diff.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    int a,b,c;
+    printf("enter two numbres : ");
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    printf("difference of two numbres is : %d",c);
+    
+
+}
+
