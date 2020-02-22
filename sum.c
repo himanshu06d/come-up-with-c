e9bd4640123c5f0e7a741b3b9bf4cb4c5889ea4c
himanshu06d <hd156681@gmail.com>
2020-02-22 17:45:57 +0530

    sum of two numbers

--- sum.c
+++ sum.c
@@ -5,7 +5,7 @@ void main()
 int a,b,c;
 
 	printf("enter the two numbers = ");
-	scanf("%d%d",&a,&b); 
+	scanf("%d%d",&a,&b); //save the value
 	c=a+b;
 	printf("the sum of two numbers is %d",&c); //print value of c
 }
