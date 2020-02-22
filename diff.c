236e7eaad7f3f3194f14fcd1e8a79053e355e6f9
himanshu06d <hd156681@gmail.com>
2020-02-22 17:37:53 +0530

    difference of two numbrers

--- diff.c
+++ diff.c
@@ -5,7 +5,7 @@ void main()
     int a,b,c; //initialize the variables
     printf("enter two numbres : ");
     scanf("%d%d",&a,&b);
-    c=a-b;
+    c=a-b; //formula
     printf("difference of two numbres is : %d",c); //print final c 
     
 
