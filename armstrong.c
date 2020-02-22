799f0aa6ed3cf68b6b06b8a065f4fa23cbd8cb41
himanshu06d <hd156681@gmail.com>
2020-02-22 17:39:35 +0530

    armstrong number

--- armstrong.c
+++ armstrong.c
@@ -5,13 +5,13 @@ int main() {
     scanf("%d", &num);
     originalNum = num;
 
-    while (originalNum != 0) {
+    while (originalNum != 0) {//loop
         remainder = originalNum % 10;
         result += remainder * remainder * remainder;
         originalNum /= 10;
     }
 
-    if (result == num)
+    if (result == num) //conditional statement
         printf("%d is an Armstrong number.", num);
     else
         printf("%d is not an Armstrong number.", num);
