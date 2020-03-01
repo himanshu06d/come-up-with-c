00dcba3837024f0c0ed306776bb477dd9d11e95a
himanshu dubey <hd156681@gmail.com>
2020-03-01 22:03:55 +0530

    command added

--- diff.c
+++ diff.c
@@ -5,7 +5,7 @@ void main()
     int a,b,c; //initialize the variables
     printf("enter two numbres : ");
     scanf("%d%d",&a,&b);
-    c=a-b; //formula
+    c=a-b; //formula of difference between two numbers
     printf("difference of two numbres is : %d",c); //print final c 
     
 
