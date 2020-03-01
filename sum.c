a6ebca505b1992e7ae0984de156027c5823e7abf
himanshu dubey <hd156681@gmail.com>
2020-03-02 01:09:42 +0530

    first

--- sum.c
+++ sum.c
@@ -4,7 +4,7 @@ void main()//include main function
 {
 int a,b,c;//int declared
 
-	printf("enter the two numbers = ");
+	printf("enter the two numbers = ");//printf statement
 	scanf("%d%d",&a,&b); //save the value
 	c=a+b;
 	printf("the sum of two numbers is %d",&c); //print value of c
