c320f013578a9354cde965271807821cc301d734
himanshu dubey <hd156681@gmail.com>
2020-03-02 01:10:39 +0530

    second branch

--- sum.c
+++ sum.c
@@ -6,6 +6,6 @@ int a,b,c;//int declared
 
 	printf("enter the two numbers = ");//printf statement
 	scanf("%d%d",&a,&b); //save the value
-	c=a+b;
+	c=a+b;//formula
 	printf("the sum of two numbers is %d",&c); //print value of c
 }//end of programme
