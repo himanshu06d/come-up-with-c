e685e447fac20b101691fdb41436793dffb4a87e
himanshu dubey <hd156681@gmail.com>
2020-04-23 13:49:55 +0530

    area of triangle

---
+++ area_triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int h,b,a;
+		printf("Enter the height of triangle = ");
+		scanf("%d",&h);
+		printf("Enter the base length = ");
+		scanf("%d",&b);
+		a=h*b/2;
+		printf("The area of triangle is %d",a/n);
+		return 0;
+}
