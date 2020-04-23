a06682dbed28fc0105a43e3d7f4d61782b7e09b7
himanshu dubey <hd156681@gmail.com>
2020-04-23 17:09:35 +0530

    average marks

---
+++ average.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	float e,m,h,s,pe,avg; 
+		printf("Enter the marks of english = "/n);
+	scanf("%d",e);
+	printf("Enter the marks of maths = "/n);
+	scabf("%d",m);
+	printf("Enter the marks of hindi = "/n);
+	scanf("%d",h);
+	printf("Enter the marks of science = "/n);
+	scanf("%d",s);
+	printf("Enter the marks od physsical education = "/n);
+	scanf("%d",pe);
+	avg=e+m+h+s+pe/5;
+
+printf("The average marks is =%d ",avg);
+return 0;
+}
