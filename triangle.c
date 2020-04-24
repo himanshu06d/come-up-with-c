2e8f1e48b818ff4f3dd643d7cf70a368cfbfc81c
himanshu dubey <hd156681@gmail.com>
2020-04-24 21:51:35 +0530

    Triangle validity

---
+++ triangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	ptintf("Enter the three values of angle of triangle=\n");
+	scanf("%f%f%f",&a,&b,&c);
+	if (a+b+c==180)
+	{
+		printf("The tirangle is valid\n");
+
+
+
+	}
+	else 
+
+		printf("NOt valid triangle\n");
+	return 0;
+}
