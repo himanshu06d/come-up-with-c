2522173da591127b037574333dda3e955d498704
himanshu dubey <hd156681@gmail.com>
2020-04-24 15:47:57 +0530

    consonent vowel

---
+++ check.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("Enter the alphabet= \n");
+scanf("%c",&ch);
+if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+{
+	ptintf("vowel");
+}
+else
+{
+	printf("consonent");
+}
+return 0;
+}
+
