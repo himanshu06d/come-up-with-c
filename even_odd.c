7aa75c6a1095cd5532d500dfc6ff952b5f35c1a1
himanshu dubey <hd156681@gmail.com>
2020-04-24 15:42:34 +0530

    even odd

---
+++ even_odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the no. :- \n");
+	scanf("%d",&a);
+	if (a%2==0)
+		ptintf("Even number\n");
+	else 
+		printf("odd number\n");
+	return 0;
+}
