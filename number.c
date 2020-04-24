ff75d095195a448c9a6b9f61f339c522e4b21d9d
himanshu dubey <hd156681@gmail.com>
2020-04-24 14:02:29 +0530

    positive negative

---
+++ number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	printf("Enter the number:- \n");
+scanf("%d",&a);
+if (a>0)
+{
+	printf("Positive");
+
+if (a<0)
+
+	printf("Negative");
+}
+else 
+printf("zero");
+return 0;
+}
