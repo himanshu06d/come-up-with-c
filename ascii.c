056b13f8f51a886cc33c684aae21a26d4397fac1
himanshu dubey <hd156681@gmail.com>
2020-04-24 21:33:52 +0530

    ascii numbers

---
+++ ascii.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter the alphabet =\n");
+	scanf("%c",&ch);
+	{
+		if (ch>=65 && ch<=90 || ch>=97 && ch<=122)
+	
+		printf("It is a alphabet\n");
+	if (ch>=48 && ch<=57)
+		printf("It is a number\n");
+	}
+	else printf("It is a special character/symbol\n");
+	
+	return 0;
+}
