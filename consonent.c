45a9a9ade361d73e066095c1754b62a191f47b4f
himanshu dubey <hd156681@gmail.com>
2020-04-26 00:21:48 +0530

    vowels and consonent

---
+++ consonent.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+
+
+
+	printf("Enter the alphabet\n");
+	scanf("%ch",&ch);
+	switch(ch)
+	{
+		case 'a':
+			printf("vowel\n");
+			break;
+		case 'e':
+			printf("vowe;\n");
+			break;
+		case 'i':
+			printf("vowel\n");
+			break;
+		case 'o':
+			printf("vowel\n");
+			break;
+		case 'u':
+			printf("vowel\n");
+			break;
+			default:
+			printf("consonent\m");
+	}
+}
