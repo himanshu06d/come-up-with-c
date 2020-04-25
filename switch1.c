61f596c8b120de2bfd7b6bd13dcc3559af155abf
himanshu dubey <hd156681@gmail.com>
2020-04-25 20:59:49 +0530

    switch case

---
+++ switch1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#int main()
+{
+	int no;
+	printf("Enter the number of day \n");
+	scanf("%d",&n);
+switch(n)
+{
+	case 1:
+		printf("Monday\n");
+		break ;
+		case 2:
+		printf("Tuesday\n");
+		break;
+		case 3:
+printf("wednesday\n");
+break;
+		case 4:
+printf("Thrusday\n");
+break;
+		case 5:
+printf("Friday\n");
+break;
+		case 6:
+printf("Saturday\n");
+break;
+		case 7:
+printf("Sunday\n");
+break;
+		default:
+printf("Invalid input");
+}
+}
